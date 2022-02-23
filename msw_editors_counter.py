4cf3ed9a7a50f535dec864428a29e791776eb076
Alexandr Katsko <alexandr.katsko@gmail.com>
2022-02-24 04:00:16 +0700

    refactoring read_xml

--- msw_editors_counter.py
+++ msw_editors_counter.py
@@ -1,14 +1,22 @@
 from os import listdir
+from pathlib import Path
 from xml.dom import minidom
 from zipfile import ZipFile
 
+base_dir = Path('/tmp/docx_dir')
 
-for filename in listdir('/tmp/docx_dir'):
+
+def read_xml(xml_file):
+    xmldoc = minidom.parse(xml_file)
+    tags = xmldoc.getElementsByTagName('cp:lastModifiedBy')
+    tag = tags[0]
+    last_modified_by = tag.firstChild.nodeValue
+    return last_modified_by
+
+
+for filename in listdir(base_dir):
     if filename.endswith('.docx'):
-        with ZipFile(f'/tmp/docx_dir/{filename}') as zip_file:
+        with ZipFile(base_dir / filename) as zip_file:
             with zip_file.open('docProps/core.xml') as xml_file:
-                xmldoc = minidom.parse(xml_file)
-                tags = xmldoc.getElementsByTagName('cp:lastModifiedBy')
-                tag = tags[0]
-                last_modified_by = tag.firstChild.nodeValue
+                last_modified_by = read_xml(xml_file)
                 print(last_modified_by)
