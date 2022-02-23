b230ca2ff2dffe44040473dfab3bc5ca28689840
Alexandr Katsko <alexandr.katsko@gmail.com>
2022-02-24 03:45:48 +0700

    read file from directory

--- msw_editors_counter.py
+++ msw_editors_counter.py
@@ -1,12 +1,14 @@
+from os import listdir
 from xml.dom import minidom
 from zipfile import ZipFile
 
 
-filename = '/tmp/docx_dir/file.docx'
-with ZipFile(filename) as zip_file:
-    with zip_file.open('docProps/core.xml') as xml_file:
-        xmldoc = minidom.parse(xml_file)
-        tags = xmldoc.getElementsByTagName('cp:lastModifiedBy')
-        tag = tags[0]
-        last_modified_by = tag.firstChild.nodeValue
-        print(last_modified_by)
+for filename in listdir('/tmp/docx_dir'):
+    if filename.endswith('.docx'):
+        with ZipFile(f'/tmp/docx_dir/{filename}') as zip_file:
+            with zip_file.open('docProps/core.xml') as xml_file:
+                xmldoc = minidom.parse(xml_file)
+                tags = xmldoc.getElementsByTagName('cp:lastModifiedBy')
+                tag = tags[0]
+                last_modified_by = tag.firstChild.nodeValue
+                print(last_modified_by)
