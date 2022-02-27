810c6e1a89c2fae62204320c3cf2f4ed0b672ab1
Alexandr Katsko <alexandr.katsko@gmail.com>
2022-02-28 03:49:13 +0700

    get_files

--- msw_editors_counter.py
+++ msw_editors_counter.py
@@ -6,6 +6,31 @@ from zipfile import ZipFile
 base_dir = Path('/tmp/docx_dir')
 
 
+def run():
+    files = get_files(base_dir)
+    editors = get_editors(files)
+    print(editors)
+
+
+def get_files(base_dir):
+    result = []
+    for filename in listdir(base_dir):
+        if filename.endswith('.docx'):
+            result.append(base_dir / filename)
+    return result
+
+
+def get_editors(files):
+    result = []
+    for filename in files:
+        with ZipFile(filename) as zip_file:
+            with zip_file.open('docProps/core.xml') as xml_file:
+                last_modified_by = read_xml(xml_file)
+                result.append(last_modified_by)
+    return result
+
+
+
 def read_xml(xml_file):
     xmldoc = minidom.parse(xml_file)
     tags = xmldoc.getElementsByTagName('cp:lastModifiedBy')
@@ -14,9 +39,4 @@ def read_xml(xml_file):
     return last_modified_by
 
 
-for filename in listdir(base_dir):
-    if filename.endswith('.docx'):
-        with ZipFile(base_dir / filename) as zip_file:
-            with zip_file.open('docProps/core.xml') as xml_file:
-                last_modified_by = read_xml(xml_file)
-                print(last_modified_by)
+run()
