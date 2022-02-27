a21160c1fdfc70f66ef1af860f5d8f34d74f8abc
Alexandr Katsko <alexandr.katsko@gmail.com>
2022-02-28 04:31:01 +0700

    get files from dir and subdir

--- msw_editors_counter.py
+++ msw_editors_counter.py
@@ -8,26 +8,36 @@ base_dir = Path('/tmp/docx_dir')
 def run():
     files = get_files(base_dir)
     editors = get_editors(files)
-    print(editors)
+    for dir_date, editor in editors:
+        print(f'{dir_date} {editor}')
 
 
 def get_files(base_dir):
     result = []
-    for filename in base_dir.iterdir():
-        if filename.suffix == '.docx':
-            result.append(base_dir / filename)
+    for dir_path in base_dir.iterdir():
+        if not dir_path.is_dir():
+            continue
+        dir_date = dir_path.name
+        for path in dir_path.iterdir():
+            # files on first level
+            if path.is_file() and path.suffix == '.docx':
+                result.append((dir_date, base_dir/path))
+            elif path.is_dir():
+                for inner_path in path.iterdir():
+                    # files on second level
+                    if inner_path.is_file() and inner_path.suffix == '.docx':
+                        result.append((dir_date, base_dir/inner_path))
     return result
 
 
 def get_editors(files):
     result = []
-    for filename in files:
+    for dir_date, filename in files:
         with ZipFile(filename) as zip_file:
             with zip_file.open('docProps/core.xml') as xml_file:
                 last_modified_by = read_xml(xml_file)
-                result.append(last_modified_by)
+                result.append((dir_date, last_modified_by))
     return result
-
 
 
 def read_xml(xml_file):
